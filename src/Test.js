aac523f9451c96985ddcd5b05ff422612f2bf3c8
kitthornton <kit.thornton16@gmail.com>
2021-02-09 21:21:58 +0100

    committed from terminal

--- src/Test.js
+++ src/Test.js
@@ -1,1 +1,44 @@
 // So here we will test adding in an additional file
+
+function Test() {
+
+    const user = {
+        firstName: 'Kit',
+        lastName: 'Thornton'
+    };
+
+    return (
+        <div>
+            {PopulateElement(user)}
+            {PopulateElement()}
+        </div>
+    );
+};
+
+function PopulateElement(user)
+{
+    let elem;
+
+    if (user){
+        elem = (
+            <div>
+                Hello {FormatUserName(user)}
+            </div>
+        )
+    } else {
+        elem = (
+            <div>
+                Hello you stranger cunt
+            </div>
+        )
+    }
+
+    return elem;
+}
+
+function FormatUserName(user)
+{
+    return user.firstName + ' ' + user.lastName;
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <div>
+          {/*<App />*/}
+          <Test />
+      </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
